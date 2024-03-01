5e4469d57e4cb4949373ee6dca31d13586a04c07
claysonAlves12 <128482372+claysonAlves12@users.noreply.github.com>
2024-03-01 11:58:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let links = document.querySelectorAll(".link");
+    links.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            let targetId = this.getAttribute("data-target");
+            let targetElement = document.getElementById(targetId);
+            let contents = document.querySelectorAll(".conteudo");
+            contents.forEach(function(content) {
+                if (content !== targetElement) {
+                    content.classList.remove("ativo");
+                }
+            });
+            targetElement.classList.toggle("ativo");
+            let activeLink = document.querySelector(".link.active");
+            if (activeLink) {
+                activeLink.classList.remove("active");
+            }
+            this.classList.add("active");
+        });
+    });
+
+    let perguntas = document.querySelectorAll('.pergunta');
+    perguntas.forEach(function(pergunta) {
+        pergunta.addEventListener('click', function() {
+            let estaAtiva = pergunta.classList.contains('active');
+
+            perguntas.forEach(function(outraPergunta) {
+                outraPergunta.classList.remove('active');
+            });
+
+            if (!estaAtiva) {
+                pergunta.classList.add('active');
+            }
+        });
+    });
+});
